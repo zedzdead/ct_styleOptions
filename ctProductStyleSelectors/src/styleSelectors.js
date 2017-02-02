508575f8df391d0ddbdeb6e938774e0d23a84890
Andy Davies <andycarldavies@gmail.com>
2017-02-02 11:49:28 +0000

    Add files via upload

--- ctProductStyleSelectors/src/styleSelectors.js
+++ ctProductStyleSelectors/src/styleSelectors.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {				
 
+	//Basic Styling - Converts drop downs to radio buttons
 	$( "#product_style_Kit_Type" ).productStyle();
 
-	/*
-	$( "#product_style_Mens_Chest" ).productStyle( {
-		//propertyName: "a custom value"					
-	});
+	//Basic Styling
+	$( "#product_style_Mens_Chest" ).productStyle();
+
+	//Style uses the prodStyles defined file, in this case uses the Shirt Colour Styles
+	//The Styles need to be set up in the Products and Categories section of the Backoffice	
 	$( "#product_style_colour" ).productStyle( {
-		//propertyName: "a custom value",
 		productStyle: "Shirt_Colour" //Case Sensetive - use underscores unless spaceAlt is declared
 	});
-	*/	
+
 })
